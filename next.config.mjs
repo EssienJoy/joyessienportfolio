620fa950a498833c60e683d79eb98e00ab37685d
essienjoy <ejoy7602@gmail.com>
2025-11-12 21:11:56 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
+    webpack: (config) => {
+        return config;
+    },
     experimental: {
         cacheComponents: true,
     }
